1f7eb2935f92b2bdbb9b3e482f8f9a5b33fcbec7
Tashmax19 <tashma19@gmail.com>
2021-11-24 15:39:37 +0300

    update - website

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  setup() {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Questrial;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+@font-face {
+  font-family: "Questrial";
+  src: url("./assets/Questrial-Regular.ttf");
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
